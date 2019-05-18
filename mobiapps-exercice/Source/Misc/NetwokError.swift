fba0dd41d7ddcfa177134a191aaa14d23cd8435f
Quentin Eude <quentineude@gmail.com>
2019-05-18 16:45:48 +0200

    Add custom error class

---
+++ mobiapps-exercice/Source/Misc/NetwokError.swift
@@ -0,0 +1,11 @@
+//
+//  NetwokError.swift
+//  mobiapps-exercice
+//
+//  Created by Quentin Eude on 15/05/2019.
+//  Copyright © 2019 Quentin Eude. All rights reserved.
+//
+
+public enum NetworkError: Error {
+    case emptyResult
+}
