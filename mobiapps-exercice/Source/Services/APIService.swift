56f14823ae6bbf0e321a6d7b61ef8432a4645edd
Quentin Eude <quentineude@gmail.com>
2019-05-18 14:07:55 +0200

    APIService : get groups

---
+++ mobiapps-exercice/Source/Services/APIService.swift
@@ -0,0 +1,44 @@
+
+//
+//  APIService.swift
+//  mobiapps-exercice
+//
+//  Created by Quentin Eude on 14/05/2019.
+//  Copyright © 2019 Quentin Eude. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class APIService {
+    
+    static func getAllGroupsIds(completion: @escaping (Result<[String], NetworkError>) -> Void){
+        AF.request("https://api.guildwars2.com/v2/achievements/groups")
+            .responseJSON { response in
+                guard let data = response.value as? [String]
+                    else{
+                        completion(.failure(.emptyResult))
+                        return
+                }
+                completion(.success(data))
+        }
+    }
+    
+    
+    static func getAllGroups(completion: @escaping (Result<[Group], Error>) -> Void){
+        getAllGroupsIds{ result in
+            switch result{
+            case .success(let ids):
+                let parameters : Parameters = [
+                    "ids" : ids.joined(separator: ",")
+                ]
+                AF.request("https://api.guildwars2.com/v2/achievements/groups", parameters:parameters)
+                    .responseDecodable{(response: DataResponse<[Group]>) in
+                        completion(response.result)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
